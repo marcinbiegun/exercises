fd8e2a73c10c934e81560ff843aaf31bc2b595f7
Marcin Biegun <marcin@mm.st>
2019-09-16 20:26:01 +0200

    Add a list flatten implementation

--- elixir-flatten/lib/flatten.ex
+++ elixir-flatten/lib/flatten.ex
@@ -1,5 +1,20 @@
 defmodule Flatten do
-  def hello(name) do
-    "Hello, #{name}!"
+  @doc """
+  Given a list, returns a new list that is (recursively) flattened.
+
+  ## Example
+    iex> Flatten.flatten([1, [2, 3]])
+    [1, 2, 3]
+    iex> Flatten.flatten([1, [2, [3]]])
+    [1, 2, 3]
+  """
+  def flatten(list) when is_list(list) do
+    list |> Enum.reduce([], fn el, acc ->
+      if is_list(el) do
+        acc ++ flatten(el)
+      else
+        acc ++ [el]
+      end
+    end)
   end
 end
--- elixir-flatten/test/flatten_test.exs
+++ elixir-flatten/test/flatten_test.exs
@@ -1,8 +1,31 @@
 defmodule FlattenTest do
   use ExUnit.Case
+  import Flatten
   doctest Flatten
 
-  test "greets the world" do
-    assert Flatten.hello() == :world
+  describe "flatten/1" do
+    test "not implemented for nil" do
+      assert_raise FunctionClauseError, fn ->
+        flatten(nil)
+      end
+    end
+
+    test "does nothing to flat list" do
+      assert flatten([1, 2, 3]) == [1, 2, 3]
+    end
+
+    test "flattens nested list" do
+      assert flatten([1, [2, 3]]) == [1, 2, 3]
+    end
+
+    test "flattens deeply nested list" do
+      assert flatten([1, [[2, 3]], [[[[4]]]]]) == [1, 2, 3, 4]
+    end
+
+    test "flattens list containg multiple variable types" do
+      given = [[1], ["hello"], [0.001], [:world], [%{weight: 500}]]
+      expected = [1, "hello", 0.001, :world, %{weight: 500}]
+      assert flatten(given) == expected
+    end
   end
 end
