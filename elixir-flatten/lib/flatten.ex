dc924c2a0b6435fcc3131ad12a5f63c2dfa95b3b
Marcin Biegun <marcin@mm.st>
2019-09-16 16:21:07 +0200

    Setup new Flatten Mix project

---
+++ elixir-flatten/lib/flatten.ex
@@ -0,0 +1,5 @@
+defmodule Flatten do
+  def hello(name) do
+    "Hello, #{name}!"
+  end
+end
---
+++ elixir-flatten/test/flatten_test.exs
@@ -0,0 +1,8 @@
+defmodule FlattenTest do
+  use ExUnit.Case
+  doctest Flatten
+
+  test "greets the world" do
+    assert Flatten.hello() == :world
+  end
+end
