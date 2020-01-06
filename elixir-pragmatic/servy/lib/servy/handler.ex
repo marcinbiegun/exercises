7284fcf020a5dd6f3398413b9ecf4a18ac70bcb4
Marcin Biegun <marcin@mm.st>
2020-01-06 14:21:30 +0100

    Code from a few more chapters

--- elixir-pragmatic/servy/lib/servy/handler.ex
+++ elixir-pragmatic/servy/lib/servy/handler.ex
@@ -1,10 +1,40 @@
 defmodule Servy.Handler do
+
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    with :ok <- authorize_author(conn, id),
+      %User{} = user <- Accounts.get_user!(id),
+      {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+        render(conn, "show.json", user: user)
+      end
+    end
+  end
+
+
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
+    |> log
     |> route
+    |> track
     |> format_response
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     [method, path, _] =
@@ -13,21 +43,67 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
 
-  def route(conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+  def route(%{ method: "GET", path: "/wildthings"} = conv) do
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(%{ method: "GET", path: "/bears"} = conv) do
+    %{ conv | status: 200,  resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(%{ method: "GET", path: "/bears/" <> id} = conv) do
+    %{ conv | status: 200,  resp_body: "Bear #{id}" }
+  end
+
+  def route(%{method: "GET", path: "/about"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  end
+
+  def route(%{ path: path } = conv) do
+    %{ conv | status: 404,  resp_body: "No #{path} here!" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "Ok",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
@@ -40,5 +116,47 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
+IO.puts response
 
+
+
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+
+
+
+
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
 IO.puts response
