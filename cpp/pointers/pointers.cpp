b7cece366533eb6d366aff02146d3bff3dcf8c52
Marcin Biegun <marcin@mm.st>
2020-02-04 17:25:13 +0100

    Update c++ pointers example

--- cpp/pointers/pointers.cpp
+++ cpp/pointers/pointers.cpp
@@ -4,19 +4,30 @@ using namespace std;
 int main()
 {
     int x = 5;
-    int *x_ptr = &x;
-    int x_val = *x_ptr;
+    int* x_ptr = &x;
+    int x_copy = *x_ptr;
+    int &x_ref = *x_ptr;
 
-    cout << x << "\n";
-    cout << x_ptr << "\n";
-    cout << x_val << "\n";
+    cout << "x == " << x << "\n";
+    cout << "x_ptr == " << x_ptr << "\n";
+    cout << "x_copy == " << x_copy << "\n";
+    cout << "x_ref == " << x_ref << "\n";
 
-    cout << "---"
-         << "\n";
-
+    cout << "\n--- after *x_ptr = 7\n";
     *x_ptr = 7;
-    cout << *x_ptr << "\n";
-    cout << x_val << "\n";
+
+    cout << "x == " << x << "\n";
+    cout << "x_ptr == " << x_ptr << "\n";
+    cout << "x_copy == " << x_copy << "\n";
+    cout << "x_ref == " << x_ref << "\n";
+
+    cout << "\n--- after x_ref = 999\n";
+    x_ref = 999;
+
+    cout << "x == " << x << "\n";
+    cout << "x_ptr == " << x_ptr << "\n";
+    cout << "x_copy == " << x_copy << "\n";
+    cout << "x_ref == " << x_ref << "\n";
 
     return 0;
 }
