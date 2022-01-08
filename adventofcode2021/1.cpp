9064227ea3062dc8677b77a39734d45165eae920
Marcin Biegun <marcin@mm.st>
2022-01-08 15:58:46 +0100

    Fix formatting

--- adventofcode2021/1.cpp
+++ adventofcode2021/1.cpp
@@ -8,7 +8,8 @@ using namespace std;
 /**
  * Returns textfile lines content as vector<string>
  */
-vector<string> getInputLines() {
+vector<string> getInputLines()
+{
   ifstream inputFile("1input.txt");
 
   // Read file
@@ -18,7 +19,8 @@ vector<string> getInputLines() {
   // Split input lines
   vector<string> strings;
   string s;
-  while (getline(buffer, s, '\n')) {
+  while (getline(buffer, s, '\n'))
+  {
     strings.push_back(s);
   }
   return strings;
@@ -27,40 +29,45 @@ vector<string> getInputLines() {
 /**
  * Converts strings to integers, skips if unable to convert an element.
  */
-vector<int> stringsToInts(vector<string> lines) {
+vector<int> stringsToInts(vector<string> lines)
+{
   vector<int> integers;
 
-  for (string line : lines) {
+  for (string line : lines)
+  {
     if (line.length() < 1)
       continue;
-    try {
+    try
+    {
       int integer = stoi(line);
       integers.push_back(integer);
     }
-    catch (std::invalid_argument& e) {
+    catch (std::invalid_argument &e)
+    {
     }
   }
 
   return integers;
 }
 
-int main() {
-    vector<string> lines = getInputLines();
-    cout << "Input lines: " << lines.size() << endl;
-    vector<int> ints = stringsToInts(lines);
+int main()
+{
+  vector<string> lines = getInputLines();
+  cout << "Input lines: " << lines.size() << endl;
+  vector<int> ints = stringsToInts(lines);
 
-    int value_inc_counter = 0;
-    for (int i = 1; i < ints.size(); i++) {
-      int prev_measurement = ints[i-1];
-      int this_measurement = ints[i];
+  int value_inc_counter = 0;
+  for (int i = 1; i < ints.size(); i++)
+  {
+    int prev_measurement = ints[i - 1];
+    int this_measurement = ints[i];
 
-      // cout << "this measurement: " << this_measurement << endl;
-      if (this_measurement > prev_measurement)
-        value_inc_counter++;
-    }
+    // cout << "this measurement: " << this_measurement << endl;
+    if (this_measurement > prev_measurement)
+      value_inc_counter++;
+  }
 
-    cout << "Values inreased times: " << value_inc_counter<< endl;
+  cout << "Values inreased times: " << value_inc_counter << endl;
 
-    return 0;
+  return 0;
 }
-
